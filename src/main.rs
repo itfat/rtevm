36c77ef04200f7d48f686fd72f8a864155a06bdc
7at <itisfor@outlook.com>
2024-07-20 12:34:39 +0500

    updated memory input

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,10 @@ fn main() {
 
     // println!("{}", stack.to_string());
 
-    // println!("--------Memory--------");
-    // let mut mem = Memory::new();
-    // mem.store(0, &[1, 2, 3, 4]);
-    // println!("{:#?}", mem.load(0));
+    println!("--------Memory--------");
+    let mut mem = Memory::new();
+    mem.store(0, &[U256::from(1), U256::from(2), U256::from(3)]);
+    println!("{:#?}", mem.load(0));
 
     // println!("--------Storage--------");
     // let mut storage = Storage::new();
@@ -39,19 +39,19 @@ fn main() {
     // println!("{:#?}", storage.load(1)); //warm access
     
 
-    println!("--------EVM--------");
-    let program = vec![0x60, 0x0d, 0x60, 0x0e, 0x01];
+    // println!("--------EVM--------");
+    // let program = vec![0x60, 0x0d, 0x60, 0x0e, 0x01];
 
-    let call_data = vec![];
-    let sender = H160::zero();  
-    let gas = 1000;  
-    let value = 0;   
+    // let call_data = vec![];
+    // let sender = H160::zero();  
+    // let gas = 1000;  
+    // let value = 0;   
 
-    // Create an instance of EVM
-    let mut evm = EVM::new(sender, gas, value, program, call_data);
-    println!("Initial EVM state: {:#?}", evm);
+    // // Create an instance of EVM
+    // let mut evm = EVM::new(sender, gas, value, program, call_data);
+    // println!("Initial EVM state: {:#?}", evm);
 
-    evm.run();
+    // evm.run();
 
-    println!("Final EVM state: {:#?}", evm);
+    // println!("Final EVM state: {:#?}", evm);
 }
